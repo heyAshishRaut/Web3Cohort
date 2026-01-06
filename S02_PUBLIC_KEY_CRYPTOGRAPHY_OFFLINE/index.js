973baccf87cdd5bdbb80d97a9c8c458e2017bebb
Ashish Raut <theashish32@gmail.com>
2026-01-06 22:35:05 +0530

    S02 - Public Private Key - watched till 23:00 mins

---
+++ S02_PUBLIC_KEY_CRYPTOGRAPHY_OFFLINE/index.js
@@ -0,0 +1,6 @@
+// const str = "h"
+// const binaryRepresentation = new TextEncoder().encode(str)
+// console.log(binaryRepresentation) // Uint8Array(1) [ 104 ]
+
+let bytes = new Uint8Array([0, 255, 127, 128]);
+console.log(bytes)
