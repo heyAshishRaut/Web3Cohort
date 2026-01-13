64f3b23ad38159f144ef987c83da8a2f1d73ccf6
Ashish Raut <theashish32@gmail.com>
2026-01-13 13:25:38 +0530

    Base58

--- S02_PUBLIC_KEY_CRYPTOGRAPHY_OFFLINE/index.js
+++ S02_PUBLIC_KEY_CRYPTOGRAPHY_OFFLINE/index.js
@@ -1,6 +1,18 @@
-// const str = "h"
-// const binaryRepresentation = new TextEncoder().encode(str)
-// console.log(binaryRepresentation) // Uint8Array(1) [ 104 ]
+// 01. What do you think happens to the first element here? Does it throw an error?
+// let uint8Arr = new Uint8Array([0, 255, 127, 128]);
+// uint8Arr[1] = 300;
 
-let bytes = new Uint8Array([0, 255, 127, 128]);
-console.log(bytes)
+// 300 mod 256 = 44
+// Uint8Array(4) [0, 44, 127, 128]
+
+// 02. What is 11001010 converted to in decimal ?
+let binary = 11001010
+let power = 0
+let value = 0
+
+while(binary > 0) {
+    let digit = binary % 10
+    value = value + (digit * Math.pow(2, power++))
+    binary = Math.floor(binary / 10)
+}
+console.log(value) // 202
